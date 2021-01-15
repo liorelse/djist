287865aa47c90beb1eaa1adcf46023675ec8ef5c
Lior Else <buggyfirmware@protonmail.com>
2021-01-14 22:10:19 -0300

    feat: message strings

--- assembler/generics/msg.py
+++ assembler/generics/msg.py
@@ -7,6 +7,7 @@ __license__ = "GPLv3"
 
 
 # General messages
+GENERAL_ERROR = 'Could not complete because %s'
 EVAL_ATTEMPT = 'Tried to evaluate expression (%s)'
 EVAL_ERROR = 'Could not evaluate because %s'
 INDEX_OUT_OF_RANGE = 'Invalid index (%s) for list/array length (%s)'
@@ -16,18 +17,12 @@ KEY_VALUE = 'Key (%s) has value (%s)'
 UNEXPECTED_TYPE = 'Unexpected data type (%s)'
 
 # Filter messages
-FILTER_DICTSORT_WARNING = 'dictsort filter could not be applied. Key (%s) was not found in all list items.'
-FILTER_DICTSORTREVERSED_WARNING = 'dictsortreversed filter could not be applied. Key (%s) was not found in all list items.'
+FILTER_DICTSORT_WARNING = 'dictsort filter: Could not be applied. Key (%s) was not found in all list items.'
+FILTER_DICTSORTREVERSED_WARNING = 'dictsortreversed filter: Could not be applied. Key (%s) was not found in all list items.'
+FILTER_GET_FILTER_ERROR = 'get_digit filter: Could not be applied. Expected digit in range 1-%s. Invalid digit (%s) given.'
+FILTER_GET_FILTER_VALUE_ERROR = 'get_digit filter: Could not be applied: %s'
 
 # Processor
 PROC_GETDATA_ERROR_NONKEY = 'Key (%s) is not in dataset, and isn\'t a number'
 PROC_GETDATA_INVALID_RETURN = 'Invalid return type (%s)'
 PROC_GETDATA_LIST_1 = 'Invalid index (%s)'
-
-
-
-
-
-
-
-
