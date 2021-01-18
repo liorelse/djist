9e2a72d77e6049aa446d086fac8e43643081b7e6
Lior Else <buggyfirmware@protonmail.com>
2021-01-17 23:00:59 -0300

    feat: additional messages

--- assembler/generics/msg.py
+++ assembler/generics/msg.py
@@ -13,16 +13,25 @@ EVAL_ERROR = 'Could not evaluate because %s'
 INDEX_OUT_OF_RANGE = 'Invalid index (%s) for list/array length (%s)'
 INVALID_DICT_KEY = 'Invalid key (%s) for dictionary/object'
 INVALID_LIST_INDEX = 'Invalid index (%s) for list/array'
+JSON_DECODE_ERROR = 'The JSON data file (%s) contains an error: %s'
 KEY_VALUE = 'Key (%s) has value (%s)'
 UNEXPECTED_TYPE = 'Unexpected data type (%s)'
 
 # Filter messages
 FILTER_GENERAL = 'Could not be applied: %s'
+FILTER_ARGUMENT_TYPE_WARNING = '%s filter: Argument type (%s) was not expected'
+FILTER_VALUE_TYPE_WARNING = '%s filter: Value type (%s) was not expected'
 FILTER_DICTSORT_WARNING = 'dictsort filter: Could not be applied. Key (%s) was not found in all list items.'
 FILTER_DICTSORTREVERSED_WARNING = 'dictsortreversed filter: Could not be applied. Key (%s) was not found in all list items.'
+FILTER_FIRST_ERROR = 'first filter: ' + FILTER_GENERAL
 FILTER_GET_FILTER_ERROR = 'get_digit filter: Could not be applied. Expected digit in range 1-%s. Invalid digit (%s) given.'
 FILTER_GET_FILTER_VALUE_ERROR = 'get_digit filter: ' + FILTER_GENERAL
 FILTER_JOIN_ERROR = 'join filter: ' + FILTER_GENERAL
+FILTER_LAST_ERROR = 'last filter: ' + FILTER_GENERAL
+FILTER_LENGTH_ERROR = 'length filter: Could not be applied. No length for object (%s)'
+FILTER_LINEBREAKS_ERROR = 'linebreaks filter: ' + FILTER_GENERAL
+FILTER_LINEBREAKSBR_ERROR = 'linebreaksbr filter: ' + FILTER_GENERAL
+FILTER_LINENUMBERS_DEFAULT = 'linenumbers filter: Default value was used for argument %s'
 
 # Processor
 PROC_GETDATA_ERROR_NONKEY = 'Key (%s) is not in dataset, and isn\'t a number'
