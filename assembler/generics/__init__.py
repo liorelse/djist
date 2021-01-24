2ca6f2d3eff15409c0efd83d38844fe651fada74
Lior Else <buggyfirmware@protonmail.com>
2021-01-24 22:44:50 +0000

    refactor: module moves

--- assembler/generics/__init__.py
+++ assembler/generics/__init__.py
@@ -1,5 +1,4 @@
 from . import core
 from . import date
 from . import file
-from . import log
 from . import msg
--- assembler/job/__init__.py
+++ assembler/job/__init__.py
@@ -1,3 +1,5 @@
 from . import assemble
+from . import config
 from . import job
+from . import log
 from . import page
