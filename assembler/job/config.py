7a7ffcfdbc499ec08a74b0ca1ad9f965aa45acf9
Lior Else <buggyfirmware@protonmail.com>
2021-01-24 22:40:50 +0000

    feat: configuration module

---
+++ assembler/job/config.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""Djist: Config
+"""
+__author__ = "llelse"
+__version__ = "0.1.0"
+__license__ = "GPLv3"
+
+
+DJIST_START = None
+DJIST_STOP = None
+
+# Logging
+LOG_CONSOLE = True
+LOG_CONSOLE_LEVEL = 'info'
+LOG_FILE = True
+LOG_FILE_LEVEL = 'debug'
+LOG_FILE_LOCATION = 'djist.log'
+
+# Input
+
+
+# Output
+
